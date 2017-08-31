71c87b9c85ffa1f02692948184013c9882450e3e
raigax9 <nishad.shah@att.com>
2017-08-31 15:41:51 -0400

    Added files to run ranger-agent
    Added a script for running ranger-agent along with
    minor changes such a requirements.txt to run setup.py
    properly and added ord_db.sql to add the ord into the
    database.
    
    Change-Id: I0d2f974be8e29bdd5fbe0dc90bf4b96beed923cf

---
+++ run_ranger-agent.sh
@@ -0,0 +1,5 @@
+sudo -H pip install -r requirements.txt
+sudo python setup.py install
+ord-dbsync
+ord-api
+ord-engine
---
+++ tools/ord_db.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS ord;
+USE ord;
+CREATE USER 'ord'@'%' IDENTIFIED BY 'PASSWORD';
+GRANT ALL ON ord.* TO ord;
+commit;
